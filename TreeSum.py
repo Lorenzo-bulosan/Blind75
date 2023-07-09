c49e05a6c88642de63803fb437a43e4c3997be44
RAZER-STEALTH\loren <lorenzo.bulosan@yahoo.co.uk>
2023-07-09 09:37:55 +0100

    Solution to Tree Sum problem

---
+++ TreeSum.py
@@ -0,0 +1,23 @@
+from Node import Node
+from TreeUtils import TreeGen
+
+def tree_sum(head: Node) -> int:
+
+    if head is None: return 0
+
+    leftSum = tree_sum(head.left)
+    rightSum = tree_sum(head.right)
+    return 1 + leftSum + rightSum
+
+def test_tree_sum():
+
+    gen = TreeGen()
+    expected = 7
+    head = gen.balanced_tree_identity()
+
+    assert expected == tree_sum(head)
+
+if __name__ == '__main__':
+    test_tree_sum()
+
+    
