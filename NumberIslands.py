baf17d3a33d12a27a3b18ce5a79143d4d6c6aaf7
RAZER-STEALTH\loren <lorenzo.bulosan@yahoo.co.uk>
2023-07-15 18:39:41 +0100

    iterative solution to number of islands

--- NumberIslands.py
+++ NumberIslands.py
@@ -1,6 +1,6 @@
 from collections import deque
 
-def number_islands(grid: list[list[str]]) -> int:
+def number_islands(grid: list[list[str]], recursive = True) -> int:
 
     if len(grid) == 0:
         return 0
@@ -14,13 +14,28 @@ def number_islands(grid: list[list[str]]) -> int:
             current = grid[r][c]
 
             if current == '1' and not (r,c) in visited:
-                dfs(r, c, grid, visited)
+
+                if recursive:
+                    dfs(r, c, grid, visited)
+                else:
+                    dfs_iterative(r, c, grid, visited)
                 num_islands += 1
 
     return num_islands 
 
-# def is_valid_neighbor():
-#     return x < 0 and y < 0 and x>len(grid)-1 and y > len(grid[0])-1 and grid[x,y] == '1' and (x,y) not in visited
+
+def dfs_iterative(i, c, grid, visited):
+
+    if i >= 0 and c >= 0 and i <= len(grid)-1 and c <= len(grid[i])-1 and grid[i][c] == '1' and (i,c) not in visited:
+
+        visited.add((i,c)) 
+        dfs_iterative(i+1, c, grid, visited)
+        dfs_iterative(i-1, c, grid, visited)
+        dfs_iterative(i, c+1, grid, visited)
+        dfs_iterative(i, c-1, grid, visited)
+
+    return
+
 
 def dfs(i, c, grid, visited) -> None:
 
@@ -72,6 +87,32 @@ def test_number_islands_returns_correct_number_when_multiple_islands():
 
     assert 3 == number_islands(grid)
 
+def test_number_islands_returns_correct_number_when_single_island_iterative():
+
+    grid = [
+        ["1","1","1","1","0"],
+        ["1","1","0","1","0"],
+        ["1","1","0","0","0"],
+        ["0","0","0","0","0"]
+    ]
+
+    # actual = number_islands(grid, False)
+
+    assert 1 == number_islands(grid, False)
+
+def test_number_islands_returns_correct_number_when_multiple_islands_iterative():
+
+    grid = [
+        ["1","0","0","1"],
+        ["0","1","0","1"]
+    ]
+
+    # actual = number_islands(grid)
+
+    assert 3 == number_islands(grid, False)
 
 test_number_islands_returns_correct_number_when_single_island()
 test_number_islands_returns_correct_number_when_multiple_islands()
+
+test_number_islands_returns_correct_number_when_single_island_iterative()
+test_number_islands_returns_correct_number_when_multiple_islands_iterative()
